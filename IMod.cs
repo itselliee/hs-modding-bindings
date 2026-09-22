28571e3f1a30d0250104f2df6955214313749bae
ellie <ellie@horizon-net.work>
2026-09-22 22:18:22 +0100

    quick upload to init the repo

---
+++ IMod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HS.Modding.SDK;
+
+public interface IMod 
+{ 
+    string Identifier { get; } 
+    string Name { get; }
+    string Description { get; }
+    string[] Authors { get; }
+    string[] Dependencies { get; } // Base is pre-included in ModLoader, it loads first.
+        
+    byte[] IconBytes { get; }
+    string IconPath { get; } // If you use this, you do not need raw bytes, you should also put the image in the root folder.
+        
+    void OnRegistration(IToolkit kit); 
+    void OnUnregistration();
+}
---
+++ IToolkit.cs
@@ -0,0 +1,7 @@
+﻿namespace HS.Modding.SDK;
+
+public interface IToolkit
+{
+    void RegisterObjects(); // TODO: Create IObject so this works
+    void UnregisterObjects();
+}
